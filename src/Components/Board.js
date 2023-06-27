41a40a2fa1bd7a219fb63a35c2ebebddb7644575
Omkar Patole <omkarpatole1799@gmail.com>
2023-06-27 21:55:09 +0530

    win game logic

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,52 +1,123 @@
+import {useState} from 'react';
 import Box from './Box';
+import Button from './UI/Button';
 
 const Board = () => {
+  const [boxState, setBoxState] = useState(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  const checkWinner = () => {
+    const winConditions = [
+      // cols
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      // rows
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      // diagonals
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let [a, b, c] of winConditions) {
+      if (
+        boxState[a] !== null &&
+        boxState[a] === boxState[b] &&
+        boxState[a] === boxState[c]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const isWinner = checkWinner();
+
+  const boxClickHandler = index => {
+    const oldState = [...boxState];
+    oldState[index] = isXTurn ? 'X' : 'O';
+    setBoxState(oldState);
+    setIsXTurn(!isXTurn);
+  };
+
+  // reset game
+  const playAgainButtonHandler = () => {
+    setBoxState(Array(9).fill(null));
+  };
+
   return (
     <>
       <div className="bg-blue-500 w-60 h-60 flex items-center justify-center text-white font-bold text-3xl">
-        <div className="">
-          <Box
-            value="0"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-          <Box
-            value="0"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-        </div>
-        <div className="">
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-        </div>
-        <div className="">
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-          <Box
-            value="X"
-            className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
-          />
-        </div>
+        {!isWinner && (
+          <>
+            <div className="">
+              <Box
+                value={boxState[0]}
+                onClick={boxClickHandler.bind(null, 0)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+              <Box
+                value={boxState[1]}
+                onClick={boxClickHandler.bind(null, 1)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+              <Box
+                value={boxState[2]}
+                onClick={boxClickHandler.bind(null, 2)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+            </div>
+            <div className="">
+              <Box
+                value={boxState[3]}
+                onClick={boxClickHandler.bind(null, 3)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+              <Box
+                value={boxState[4]}
+                onClick={boxClickHandler.bind(null, 4)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+              <Box
+                value={boxState[5]}
+                onClick={boxClickHandler.bind(null, 5)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+            </div>
+            <div className="">
+              <Box
+                value={boxState[6]}
+                onClick={boxClickHandler.bind(null, 6)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+              <Box
+                value={boxState[7]}
+                onClick={boxClickHandler.bind(null, 7)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+              <Box
+                value={boxState[8]}
+                onClick={boxClickHandler.bind(null, 8)}
+                className="bg-blue-400 w-20 h-20 flex items-center justify-center border-2"
+              />
+            </div>
+          </>
+        )}
       </div>
+
+      {isWinner && (
+        <>
+          <h5>{!isXTurn ? 'X' : 'O'} Wins</h5>
+          <div>
+            <h4>Game over</h4>
+            <Button onClick={playAgainButtonHandler}>Play again!</Button>
+          </div>
+        </>
+      )}
     </>
   );
 };
